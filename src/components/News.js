c507a500a4ee162797debcb370a718939fcc3b04
sachinattri4 <150787925+sachinattri4@users.noreply.github.com>
2023-11-21 00:27:48 +0530

    add category

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+
 
 export default class News extends Component {
+  static defaultProps = {
+    country:'in',
+    pageSize: 5,
+    category:'general'
+  }
+
+  static propTypes = {
+    country:PropTypes.string,
+    pageSize:PropTypes.number,
+    category:PropTypes.string
+  }
   constructor() {
     super();
     this.state = {
@@ -12,46 +26,58 @@ export default class News extends Component {
   }
 
   async componentDidMount() {
-    //this.loading = true;
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f277e0ec0d94955948061989538d447&page=${this.state.page}&pageSize=20`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2f277e0ec0d94955948061989538d447&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({
+      loading: true
+    })
     let data = await fetch(url);
     let parseData = await data.json();
-    this.setState({ articles: parseData.articles, totalResults: parseData.totalResults });
-    //this.loading = false;
+    this.setState({
+      articles: parseData.articles,
+      totalResults: parseData.totalResults,
+      loading: false
+    });
   }
 
-  handlePreClick = async ()=> {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f277e0ec0d94955948061989538d447&page=${this.state.page - 1}&pageSize=20`;
+  handlePreClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2f277e0ec0d94955948061989538d447&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({
+      loading: true
+    })
     let data = await fetch(url);
     let parseData = await data.json();
     this.setState({
       page: this.state.page - 1,
-      articles: parseData.articles
+      articles: parseData.articles,
+      loading: false
     });
-  }
+  };
 
-  handleNextClick = async ()=> {
-    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
-      
-    }
-    else{
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f277e0ec0d94955948061989538d447&page=${this.state.page + 1}&pageSize=20`;
+  handleNextClick = async () => {
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2f277e0ec0d94955948061989538d447&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({
+        loading: true
+      })
       let data = await fetch(url);
       let parseData = await data.json();
       this.setState({
         page: this.state.page + 1,
-        articles: parseData.articles
+        articles: parseData.articles,
+        loading: false
       });
-    }
-    
-  }
+  };
 
   render() {
     return (
       <div className="container my-3">
         <h1 className="text-center">News - Top Headlines</h1>
+        { this.state.loading && <Spinner></Spinner>}
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
@@ -77,11 +103,13 @@ export default class News extends Component {
               &larr; Pre
             </button>
             <button
+              disabled={
+                this.state.page + 1 > Math.ceil(this.state.totalResults / 20)
+              }
               type="button"
               className="btn btn-dark"
               onClick={this.handleNextClick}
             >
-              {" "}
               Next &rarr;
             </button>
           </div>
